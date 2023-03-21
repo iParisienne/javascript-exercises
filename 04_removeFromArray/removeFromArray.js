407084d3a0c7add49438bf9333c72788b9cd0e3f
Parisienne <96126061+iParisienne@users.noreply.github.com>
2023-03-21 11:54:57 +0100

    Finish exercise 01-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numbers) {
+  const arrCheck = [...numbers].sort();
 
+	for(let i = 0; i < arr.length; i++){
+    for(let j = 0; j < arrCheck.length; j++){
+      if(arr[i] === arrCheck[j]){
+        arr.splice(arr.indexOf(arr[i]), 1);
+      }
+    }
+  }
+
+  return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+	if(a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number")
+    return "ERROR";
+  
+  const arr = [a, b];
+  arr.sort();
+  let num1 = arr[0];
+  let num2 = arr[1];
+  let total = 0;
 
+  for(num1; num1 <= num2; num1++){
+    total += num1;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
